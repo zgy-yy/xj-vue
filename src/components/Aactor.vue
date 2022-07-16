ea50e7f6062430eae2c54e72a0975b205c1d196a
zgy_yy <1349604075@qq.com>
2022-07-16 15:43:14 +0800

    频道页

---
+++ src/components/Aactor.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import {Actor} from "../types";
+
+const props = defineProps<{
+  actor:Actor
+}>()
+</script>
+
+<template>
+  <div class="actor">
+    <img :src="actor.coverpic"  alt="actor">
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
